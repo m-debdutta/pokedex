c384fc4585c638c564ed5a2f1a5006a2492753d4
Debdutta <m-debdutta@users.noreply.github.com>
2023-06-22 15:25:52 +0530

    attributes of pokemon splits into two

--- templates/pokemon-card-template.js
+++ templates/pokemon-card-template.js
@@ -3,71 +3,97 @@ const fs = require('fs');
 const NEWLINE = '\n';
 const EMPTY = '';
 
-const htmlTableTemplate = ({ id, types, weight, hp, xp, names, attack, defense }) => {
-  return `<section class="table">
-            <div class="table-row">
-              <div class="img">
-              <a><img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png"/></a>
-              </div>
+
+const a = () => {
+  `<section class="card">
+            <div class="img">
+              <a
+                ><img
+                  src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png"
+              /></a>
             </div>
-            <div class="table-row">
-              <div class="pokemon-name">
-                ${names}
+            <h2 class="pokemon-name">Bulbasaur</h2>
+            <div class="attributes-container">
+              <div class="types attribute">
+                <h3>Type:</h3>
+                Grass Poison
               </div>
-            </div>
-            <div class="attributes">
-              <table>
-                <tr>
-                  <th>Types</th>
-                  <td>${types}</td>
-                </tr>
-                <tr>
-                  <th>Weight</th>
-                  <td>${weight}</td>
-                </tr>
-                <tr>
-                  <th>HP</th>
-                  <td>${hp}</td>
-                </tr>
-                <tr>
-                  <th>XP</th>
-                  <td>${xp}</td>
-                </tr>
-                <tr>
-                  <th>Attack</th>
-                  <td>${attack}</td>
-                </tr>
-                <tr>
-                  <th>Defense</th>
-                  <td>${defense}</td>
-                </tr>
-              </table>
+              <div class="weight attribute">
+                <h3>Weight:</h3>
+                69
+              </div>
+              <div class="hp attribute">
+                <h3>HP:</h3>
+                45
+              </div>
+              <div class="xp attribute">
+                <h3>XP:</h3>
+                64
+              </div>
+              <div class="attack attribute">
+                <h3>Attack:</h3>
+                49
+              </div>
+              <div class="defence attribute">
+                <h3>Defence:</h3>
+                49
+              </div>
             </div>
           </section>`
+}
+
+const htmlTableTemplate = ({ id, types, weight, hp, xp, names, attack, defense }) => {
+  return `<section class="card">
+  <div class="img">
+    <a
+      ><img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png"
+    /></a>
+  </div>
+  <h2 class="pokemon-name">${names}</h2>
+  <div class="attributes-container">
+    <div class="types attribute">
+      <h3>Type:</h3>
+      ${types}
+    </div>
+    <div class="weight attribute">
+      <h3>Weight:</h3>
+      ${weight}
+    </div>
+    <div class="hp attribute">
+      <h3>HP:</h3>
+      ${hp}
+    </div>
+    <div class="xp attribute">
+      <h3>XP:</h3>
+      ${xp}
+    </div>
+    <div class="attack attribute">
+      <h3>Attack:</h3>
+      ${attack}
+    </div>
+    <div class="defence attribute">
+      <h3>Defence:</h3>
+      ${defense}
+    </div>
+  </div>
+</section>`
 };
 
 const templateImplementer = (data) => {
   const lines = data.split(NEWLINE);
-  let section = EMPTY;
-  let htmlCode = lines.slice(1).reduce((htmlCodeContext, line) => {
+  return lines.slice(1).reduce((htmlCodeContext, line) => {
     const [idNum, names, types, speed, hp, xp, attack, defense, weight] = line.split('|');
     const id = '0'.repeat(3 - idNum.length) + idNum;
     const table = htmlTableTemplate({ id, types, names, hp, xp, attack, defense, weight })
-    section += NEWLINE + table;
-    if (+id % 4 === 0) {
-      htmlCodeContext += `<section class="chart-row">${section}${NEWLINE}</section>${NEWLINE}`;
-      section = EMPTY;
-    }
+    htmlCodeContext += NEWLINE + table;
     return htmlCodeContext;
   }, EMPTY);
-
-  if (section !== EMPTY) {
-    htmlCode += `<section class="chart-row">${section}${NEWLINE}</section>${NEWLINE}`;
-  }
-  return htmlCode;
 };
 
-fs.readFile("./resources/sample-file-demo.txt", 'utf-8', (err, data) => {
+fs.readFile("./resources/sample-file.txt", 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
   console.log(templateImplementer(data));
 });
-
